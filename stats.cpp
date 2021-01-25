3744de8057191a77acd1cb62f3742d0e6b8c9f44
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-01-25 16:39:58 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,23 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>&g ) {
     //Implement statistics here
+    float Max,Min,avg = 0;
+    int Maxsize = g.size();
+    
+    Max = Min = g.begin();
+    for(auto i= g.begin();i != g.end(); i++)
+    {
+        if(Max < *i)
+        {
+            Max = *i;
+        }
+        if(Min > *i)
+        {
+            Min = *i;
+        }
+        avg = avg + *i;
+    }
+    
+    avg = avg/Maxsize;
 }
