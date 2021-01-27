feafec0a7eb2eaa8e48af5770baed6a93aa6cec1
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-01-27 16:23:20 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -8,27 +8,47 @@ float min;
 float max;
 };
 
-class IAlerter{};
+class IAlerter{
+public:
+    virtual void alretsystem() = 0;
+};
 
-class EmailAlert
+class EmailAlert:public IAlerter
 {
-    public:
+   private :
     bool emailSent;
+   public:
+    void alretsystem()
+    {
+        //Send email
+        emailSent = TRUE;
+    }
 };
 
-class LEDAlert
-{
-    public:
-    bool ledGlows;
-};
-
-class StatsAlerter
+class LEDAlert:public IAlerter
 {
     private :
-    float Max_value;
+     bool ledGlows;
+    public:
+        void alretsystem()
+    {
+        //Turn On LED
+        ledGlows = TRUE;
+    }
+};
+
+class StatsAlerter:public IAlerter
+{
+    private :
+     float Max_value;
+     std::vector<IAlerter*> Alert;
  public:
-    StatsAlerter(const float Max_value,const std::vector<IAlerter**> Alert);
-    void checkAndAlert(const std::vector<float>&value);
+    StatsAlerter(const float &M,std::vector<IAlerter*> &A)
+    {
+    Max_value = M;
+    Alert = A;
+    }
+    void checkAndAlert(const std::vector<float>& );
 };
 
 namespace Statistics {
